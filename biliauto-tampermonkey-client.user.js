81a68014e78d682b37542809e9de10bf34701472
under-the-ocean <under-the-ocean@users.noreply.github.com>
2026-06-18 16:44:57 +0800

    v0.1.0: bump version

--- biliauto-tampermonkey-client.user.js
+++ biliauto-tampermonkey-client.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BiliAutoClicker - 油猴客户端
 // @namespace    https://github.com/under-the-ocean
-// @version      0.9.3
+// @version      0.1.0
 // @match        https://www.bilibili.com/blackboard/era/award-exchange.html?*
 // @connect      bili.982835785.xyz
 // @connect      api.live.bilibili.com
@@ -69,7 +69,7 @@
     DEFAULT_START_TIME: '00:29:57',
     MAX_RELOAD_ATTEMPTS: 3,
 
-    VERSION: '0.9.3',
+    VERSION: '0.1.0',
     RETRY_COUNT: 2,
     DEBUG: true
   };
